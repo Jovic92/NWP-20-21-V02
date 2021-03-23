fd8ebe7755aac58f74ea2ad008a16f4f8d183b4f
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2021-03-23 12:54:09 +0100

    task 1 done

--- src/app/products/product-item/product-item.component.ts
+++ src/app/products/product-item/product-item.component.ts
@@ -9,7 +9,8 @@ import { Product } from 'src/app/model/product';
 export class ProductItemComponent implements OnInit {
 
   public product: Product;
-
+  public product2: Product;
+  public product3: Product;
   constructor() { }
 
   ngOnInit(): void {
@@ -19,6 +20,20 @@ export class ProductItemComponent implements OnInit {
       isOnSale: true,
       quantity: 0,
       imageUrl: "https://img.gigatron.rs/img/products/large/image5b854e7ad91cf.png"
+    }
+    this.product2 = {
+      name: "Monitor ViewSonic",
+      price: 15000,
+      isOnSale: false,
+      quantity: 0,
+      imageUrl: "https://www.winwin.rs/media/catalog/product/420x420/860/94/8609418.jpg"
+    }
+    this.product3 = {
+      name: "Benq",
+      price: 27000,
+      isOnSale: true,
+      quantity: 0,
+      imageUrl: "https://www.winwin.rs/media/catalog/product/420x420/865/47/8654761.jpg"
     }
   }
 
